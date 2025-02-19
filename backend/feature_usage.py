91a0df9db13927c40546f68a0977141e26d61725
Logan-Kelsch <7logankelsch5@gmail.com>
2025-02-19 13:04:25 -0500

    Create feature_usage.py with info box

---
+++ backend/feature_usage.py
@@ -0,0 +1,5 @@
+'''
+Logan Kelsch - 2/19/25 - This file will be used for designing and applying transformations to provided data.
+This file is robust, containing a large number of functions. Examples would include :
+LSTM formation, custom time-series image manipulation, clustering, grouping, compressing, augmentation,etc.
+'''
