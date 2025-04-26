f2e7b6a2bd034b3d43323af65023dc5ec0cb0249
Jairik <jairikmc@gmail.com>
2025-04-25 22:29:44 -0400

    Cleaning dev files for production
    Removing any unnecessary files for production, such as previous tests for the cameras or dependencies. They are now nested within the pi-dev branch, with this being the production branch

--- rgb-camera/picture-tests/take-picture.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Test program that simply takes a picture - used for demo
-
-FILENAME="capture_$(date +%Y-%m-%d_%H-%M-%S).jpg"
-libcamera-jpeg -o "$FILENAME" --width 1920 --height 1080
-echo "Picture saved as $FILENAME"
-
-# TO ACCESS - feh <picture name>
